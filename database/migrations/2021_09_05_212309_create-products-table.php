11af12aa5f420ddacf20e76a88e8cfae438e5662
Hafsa Mohamed <hafsa.mohamed1999@gmail.com>
2021-09-05 23:48:28 +0200

    migrated products table

---
+++ database/migrations/2021_09_05_212309_create-products-table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->decimal('price', $precision = 8, $scale = 2);
+            $table->binary('image');
+            $table->integer('number of');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
