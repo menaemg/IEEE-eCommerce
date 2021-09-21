53b73abe7386cd6a7390c2272d603d6a56db2374
abdalmonaem <abdalmonaem.gamal@gmail.com>
2021-09-21 10:25:08 +0200

    update

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Storage;
 
 class ProductFactory extends Factory
 {
