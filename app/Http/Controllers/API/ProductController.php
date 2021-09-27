0772773b13db48553f2e750c68b00a9aa14393d2
maha eid <mahaeid@github.com>
2021-09-27 14:02:41 -0700

    build api

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -1,19 +1,26 @@
 <?php
-
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    // get all products "api/products"
-    public function index(){
+    // get all products "api/product"
+    public function index(): \Illuminate\Http\JsonResponse
+    {
+
+       $products= Product::all();
+        return response()->json($products);
+
 
     }
 
     // get single product "api/product/{id}"
-    public function show($id){
-        
+    public function show($id): \Illuminate\Http\JsonResponse
+    {
+     $products = Product::findOrFail($id);
+      return response()->json($products);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products', [ProductController::class , 'index']);
+Route::get('/product', [ProductController::class , 'index']);
 Route::get('/product/{id}', [ProductController::class , 'show']);
